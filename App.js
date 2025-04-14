7525fec8627e4806cd29359812acdbfff16f04d3
iyanoh129 <vannykide@gmail.com>
2025-04-14 18:40:46 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './styles/App.css';
+import Hero from './components/Hero';
+import About from './components/About';
+import Services from './components/Services';
+import Gallery from './components/Gallery';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Gallery />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
